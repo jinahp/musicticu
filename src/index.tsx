a51e61af0378b1036a91c7fe4f9a77bd130824b2
Jinah <monsatfri@gmail.com>
2024-01-31 05:14:10 +0900

    init: 에디터 설정 파일 추가 및 아스키아트 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from 'src/App';
 import reportWebVitals from './reportWebVitals';
 
+console.log(`
+ ╭ ◜◝ ͡ ◜◝ ͡ ◜◝ ͡ ◜◝ ╮
+🩵 M U S I C T I C U 🩵
+ ╰ ◟◞ ͜ ◟ ͜ ◟◞ ͜ ◟◞◟◞╯
+⠀⠀⠀⠀O
+⠀⠀⠀⠀⠀°
+〃o　 ()＿()🎀
+‎⊂⌒ （´ ^ ﻌ ^）
+  ヽ_っ＿/￣￣￣/
+　 　 ＼/＿＿＿/
+`);
+
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
